4c00e6dc01a94e24e4e510d8254e4a4a343264fa
1st_MAN <1st_MAN@mail.ru>
2015-08-06 17:07:42 +0300

    Checkbox component

---
+++ client/components/semantic/checkbox.jsx
@@ -0,0 +1,48 @@
+if (typeof Semantic === 'undefined') Semantic = {};
+
+Semantic.Checkbox = React.createClass({
+  mixins: [Formsy.Mixin],
+  getDefaultProps() {
+    return {
+      type: 'checkbox',
+      mode: 'toggle'
+    }
+  },
+
+  componentDidMount() {
+    var $that=this;
+    $(this.getDOMNode()).find('.ui.checkbox').checkbox({
+      onChecked: function() {
+        $that.setValue(1);
+      },
+      onUnchecked: function() {
+        $that.setValue(undefined);
+      },
+    });
+  },
+  render() {
+    classes = [ 'field' ];
+    if (this.showRequired())  classes.push('required');
+    if (this.showError())     classes.push('error');
+    if (this.props.showInline)     classes.push('inline');
+    if (this.props.className) classes.push(this.props.className);
+
+
+    errorMessage = this.getErrorMessage();
+    input = <input {...this.props} value={this.getValue()} />;
+
+    return (
+      <div className={classes.join(' ')}>
+
+          <div className={"ui " + this.props.mode + " " + this.props.type}>
+            {this.props.label ?
+              <label>
+                {this.props.label}
+              </label>
+            : ""}
+            {input}
+          </div>
+      </div>
+    );
+  }
+});
