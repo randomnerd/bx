9f4efbbd947390838cbe91b953ac8b4c47ed49cd
1st_MAN <1st_MAN@mail.ru>
2015-08-12 14:15:22 +0300

    semantic select

---
+++ client/components/semantic/select.jsx
@@ -0,0 +1,44 @@
+if (typeof Semantic === 'undefined') Semantic = {};
+
+Semantic.Select = React.createClass({
+  mixins: [Formsy.Mixin],
+  getDefaultProps() {
+    return {
+      icon: 'search',
+      type: 'text',
+      iconSide: 'left'
+    }
+  },
+
+  componentDidMount() {
+    $(this.refs.select.getDOMNode()).dropdown();
+  },
+
+  renderOptions(){
+    return this.props.content.map((item) => {
+      return  (
+        <option key={item._id} value={item._id}>{item.title}</option>
+      );
+    });
+  },
+
+  render() {
+    classes = [ 'field' ];
+    if (this.showRequired())  classes.push('required');
+    if (this.showError())     classes.push('error');
+    if (this.props.showInline)     classes.push('inline');
+    if (this.props.className) classes.push(this.props.className);
+
+
+    errorMessage = this.getErrorMessage();
+
+
+    return (
+      <div className={classes.join(' ')}>
+        <select className="ui search dropdown" ref="select">
+          {this.renderOptions()}
+        </select>
+      </div>
+    );
+  }
+});
