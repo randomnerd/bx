b66a37e2906fa24618e6bb602cba8ce79ed3a449
1st_MAN <1st_MAN@mail.ru>
2015-08-15 22:44:59 +0300

    settings mini fix

--- client/components/user/settings.jsx
+++ client/components/user/settings.jsx
@@ -28,10 +28,10 @@ SettingsPage = React.createClass({
           <Formsy.Form key={this.props.k} className="ui form" onValidSubmit={this.newPassword} onValid={this.allowSubmit} onInvalid={this.disallowSubmit} ref='chat'>
             <Semantic.Input name="chat_name" icon="user" label="Chat name" validations="minLength:3" placeholder="Enter yor chat name" action={this.saveName()} buttonName="Save" required />
           </Formsy.Form>
+        </div>
+        <div className="ui small blue segment">
           <Formsy.Form key={this.props.k+3} className="ui form" onValidSubmit={this.newPassword} onValid={this.allowSubmit} onInvalid={this.disallowSubmit} ref='auth'>
-            <div className="two fields">
-              <Semantic.Checkbox name="confirmation" label="Don't ask for order confirmation" isChecked={this.published} />
-            </div>
+            <Semantic.Checkbox name="confirmation" label="Don't ask for order confirmation" isChecked={this.published} />
             <Semantic.Input name="new_pass" label="New password" validations="minLength:3" placeholder="Enter new password" required />
             <Semantic.Input name="confirm_pass" label="Confirm new password" validations="minLength:3" placeholder="Enter new password" required />
 
