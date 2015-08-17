ba43e148f7a5fa55346c45cd85dece07e38d4889
1st_MAN <1st_MAN@mail.ru>
2015-08-17 14:35:19 +0300

    settings view

--- client/components/user/settings.jsx
+++ client/components/user/settings.jsx
@@ -26,22 +26,48 @@ SettingsPage = React.createClass({
         </div>
         <div className="ui small blue segment">
           <Formsy.Form key={this.props.k} className="ui form" onValidSubmit={this.newPassword} onValid={this.allowSubmit} onInvalid={this.disallowSubmit} ref='chat'>
-            <Semantic.Input name="chat_name" icon="user" label="Chat name" validations="minLength:3" placeholder="Enter yor chat name" action={this.saveName()} actionButton buttonName="Save" required />
+            <Semantic.Input name="chat_name" icon="user" label="Chat name" validations="minLength:3" placeholder="Enter yor chat name" buttonAction={this.saveName} actionButton buttonName="Save" required />
           </Formsy.Form>
         </div>
         <div className="ui small blue segment">
           <Formsy.Form key={this.props.k+3} className="ui form" onValidSubmit={this.newPassword} onValid={this.allowSubmit} onInvalid={this.disallowSubmit} ref='auth'>
             <Semantic.Checkbox name="confirmation" label="Don't ask for order confirmation" isChecked={this.published} />
-            <Semantic.Input name="new_pass" label="New password" validations="minLength:3" placeholder="Enter new password" required />
-            <Semantic.Input name="confirm_pass" label="Confirm new password" validations="minLength:3" placeholder="Enter new password" required />
+            <h2 className="ui header">Two-factor authentication</h2>
+            <p>
+              Scan this QR code with
+              <a target="_blank" href="https://support.google.com/accounts/answer/1066447"> Google Authenticator</a>
+            </p>
+            <p>
+              <img src="https://chart.googleapis.com/chart?chs=200x200&chld=M|0&cht=qr&chl=otpauth://totp/CoinEx-1st_man@mail.ru?secret=3jvq42j57qpecw3n" />
+            </p>
+            <Semantic.Input name="qr_code" validations="minLength:3" placeholder="and type your code here" required />
 
             <div className="field">
-              <a className="ui positive labeled icon button" onClick={this.twoFactorAuth()}>
-                <i className="checkmark icon" />
-                Save currency
+              <a className="ui blue labeled icon button" onClick={this.twoFactorAuth()}>
+                <i className="refresh icon" />
+                Or generate a new key
               </a>
 
             </div>
+          </Formsy.Form>
+        </div>
+        <div className="ui small blue segment">
+          <div className="ui two column grid">
+            <div className="column">
+              <h2 className="ui header">
+                API Access
+              </h2>
+            </div>
+            <div className="right aligned column">
+              <a className="ui blue labeled icon button" onClick={this.twoFactorAuth()}>
+                <i className="refresh icon" />
+                Generate new API key pair
+              </a>
+            </div>
+          </div>
+          <Formsy.Form key={this.props.k+5} className="ui form" onValidSubmit={this.newPassword} onValid={this.allowSubmit} onInvalid={this.disallowSubmit} ref='api'>
+            <Semantic.Input name="qr_code" label="API key" validations="minLength:3" placeholder="and type your code here" required />
+            <Semantic.Input name="chat_name" label="API Secret" validations="minLength:3" placeholder="Enter yor chat name" buttonAction={this.saveName} actionButton buttonName="Save" required />
           </Formsy.Form>
         </div>
       </div>
