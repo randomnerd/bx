1319cda7e024643b3281468ed144f7d89f46b687
Random Nerd <randomnerd@sickoffice.net>
2015-08-21 12:08:35 +0300

    add collections

--- both/collections.js
+++ both/collections.js
@@ -1,4 +1,6 @@
 Currencies = new Mongo.Collection('currencies');
 TradePairs = new Mongo.Collection('tradepairs');
 Wallets = new Mongo.Collection('wallets');
+Transactions = new Mongo.Collection('transactions');
+BalanceChanges = new Mongo.Collection('balance_changes');
 Notifications = new Mongo.Collection('notifications');
