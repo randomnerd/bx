d03ebf332fe821bdebcd83a8f214282c209380f8
1st_MAN <1st_MAN@mail.ru>
2015-10-07 14:29:48 +0300

    Landing 1-st slide

--- app/components/home.jsx
+++ app/components/home.jsx
@@ -7,22 +7,27 @@ export default React.createClass({
         <div className="block background">
           <video id="video_background"  loop="loop" autoPlay="autoPlay" preload="auto" onended="this.play()">
             <source type="video/mp4" src="/bg.mp4"></source>
-
           </video>
         </div>
         <div className="block opacity">
           <div className="promo">
-            <h2>Bitcoin Trading You Can Trust</h2>
+            <h1>Bitcoin Trading You Can Trust</h1>
             <p>Coinbase Exchange is the leading platform to trade bitcoin.</p>
 
             <div>
-              <a className="btn login-small signin_btn" target="_blank" data-href="signup_url" href="">create account</a>
+              <a className="ui normal big teal button" target="_blank" data-href="signup_url" href="">create account</a>
 
-              <a href="/trade" className="btn login-small signup_btn">View Demo</a>
+              <a href="/trade" className="ui white big button">View Demo</a>
 
-              <button className="btn login-small mobile-login">Sign in with Coinbase</button>
+              <button className="ui normal big positive button hidden">Sign in with Coinbase</button>
             </div>
           </div>
+        </div>
+        <div className="block white">
+          dfsgdfg
+        </div>
+        <div className="block ">
+          dfsgdfg
         </div>
         <div className="block white">
           dfsgdfg
