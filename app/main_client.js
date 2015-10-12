14f7a977cf8b6212be8c728d44389facde7a0181
Random Nerd <randomnerd@sickoffice.net>
2015-10-12 13:34:25 +0300

    deploy fixes

--- app/main_client.js
+++ app/main_client.js
@@ -1,7 +1,5 @@
 import React from 'react';
 window.React = React;
-import './stylesheets/main.css';
-import './stylesheets/spinkit.css';
 import './routes';
 import './models';
 import './startup/user_subscriptions';
