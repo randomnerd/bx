ac06c943ba5d9bf9a01ffc90a5c37f78ea173e64
Johndoe <johndoe@sickoffice.net>
2015-10-14 23:29:36 +0500

    raw data chart canvas fix

--- app/components/bitindex/stockcharts/CandleStickStockScaleChartWithVolumeHistogramV3.jsx
+++ app/components/bitindex/stockcharts/CandleStickStockScaleChartWithVolumeHistogramV3.jsx
@@ -135,7 +135,7 @@ export default React.createClass({
     rawData = this.state.data;
     return (
 
-      <ChartCanvas width={this.state.width} height={this.props.height}
+      <ChartCanvas  ref='chartCanvas' width={this.state.width} height={this.props.height}
       				margin={{left: 70, right: 70, top:10, bottom: 30}} initialDisplay={200}
       				dataTransform={[ { transform: StockscaleTransformer } ]}
       				data={rawData} type={type}>
