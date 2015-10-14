d54b9a7722eb449506f0aaf40dbf45a436fa64b6
Johndoe <johndoe@sickoffice.net>
2015-10-14 20:02:25 +0500

    code style fix #9

--- app/components/user/password.jsx
+++ app/components/user/password.jsx
@@ -15,38 +15,42 @@ export default React.createClass({
       user: Meteor.user()
     };
   },
-  newPassword(){
-    var {old_pass, password} = this.refs.pass.getCurrentValues();
-    console.log(password)
+  newPassword() {
+    let {old_pass, password} = this.refs.pass.getCurrentValues();
+    console.log(password);
     Accounts.changePassword(old_pass, password, (err)=>{
       if (err) {
-        Dispatcher.dispatch({ actionType: 'NEW_NOTIFICATION', payload: { message: {_id:'pass_not_changed',type:'error',icon:'error',title:'Error!',message:err.message,timeout:3000, needShow:true} } });
-        console.log(err.message)
+        Dispatcher.dispatch({ actionType: 'NEW_NOTIFICATION', payload:
+        { message: {_id: 'pass_not_changed', type: 'error', icon: 'error', title: 'Error!',
+        message: err.message, timeout: 3000, needShow: true } } });
+        console.log(err.message);
         this.setState({errorMessage: err.message});
       } else {
-        Dispatcher.dispatch({ actionType: 'NEW_NOTIFICATION', payload: { message: {_id:'pass_changed',type:'accept',icon:'accept',title:'Your password changed!',timeout:3000, needShow:true} } });
+        Dispatcher.dispatch({ actionType: 'NEW_NOTIFICATION', payload:
+        { message: {_id: 'pass_changed', type: 'accept', icon: 'accept',
+        title: 'Your password changed!', timeout: 3000, needShow: true} } });
         this.refs.pass.reset();
       }
-    })
+    });
   },
-  allowSubmit() { this.setState({allowSubmit: true}) },
-  disallowSubmit() { this.setState({allowSubmit: false}) },
+  allowSubmit() { this.setState({allowSubmit: true}); },
+  disallowSubmit() { this.setState({allowSubmit: false}); },
 
   render() {
     return (
-      <div className="ui segments">
-        <div className="ui secondary segment">
+      <div className='ui segments'>
+        <div className='ui secondary segment'>
           <h4>Change password</h4>
         </div>
-        <div className="ui small blue segment">
-          <Formsy.Form key={this.props.k} className="ui form" onValidSubmit={this.newPassword} onValid={this.allowSubmit} onInvalid={this.disallowSubmit} ref='pass'>
-            <Semantic.Input name="old_pass" ref="old_password" type="password" label="Old password" validations="minLength:3" placeholder="Enter old password" required />
-            <Semantic.Input name="password" ref="password" validations="passwordConfirmationMatch" type="password" label="New password" placeholder="Enter new password" required />
-            <Semantic.Input name="password_confirm" ref="password_confirm" validations="passwordConfirmationMatch" type="password" label="Confirm new password" placeholder="Re-enter new password" required />
+        <div className='ui small blue segment'>
+          <Formsy.Form key={this.props.k} className='ui form' onValidSubmit={this.newPassword} onValid={this.allowSubmit} onInvalid={this.disallowSubmit} ref='pass'>
+            <Semantic.Input name='old_pass' ref='old_password' type='password' label='Old password' validations='minLength:3' placeholder='Enter old password' required />
+            <Semantic.Input name='password' ref='password' validations='passwordConfirmationMatch' type='password' label='New password' placeholder='Enter new password' required />
+            <Semantic.Input name='password_confirm' ref='password_confirm' validations='passwordConfirmationMatch' type='password' label='Confirm new password' placeholder='Re-enter new password' required />
 
-            <div className="field">
-              <a className={"ui" + (!this.state.allowSubmit ? " disabled" : '')+ " positive labeled icon button"} onClick={this.newPassword}>
-                <i className="checkmark icon" />
+            <div className='field'>
+              <a className={'ui' + (!this.state.allowSubmit ? ' disabled' : '')+ ' positive labeled icon button'} onClick={this.newPassword}>
+                <i className='checkmark icon' />
                 Save new password
               </a>
 
--- app/components/user/wallet_one.jsx
+++ app/components/user/wallet_one.jsx
@@ -11,14 +11,14 @@ export default React.createClass({
     };
   },
   newWallet(item, event) {
-    if (!Meteor.user()) { return }
-    var el = $(event.currentTarget);
+    if (!Meteor.user()) { return; }
+    let el = $(event.currentTarget);
     el.addClass('loading');
     el.attr('disabled', true);
     Meteor.call('jobs/wallet/newWallet', item._id, () => {
-        el.removeClass('loading');
-        el.attr('disabled', false);
-    })
+      el.removeClass('loading');
+      el.attr('disabled', false);
+    });
   },
   getAddress() {
     return this.data.wallet && this.data.wallet.address;
@@ -27,56 +27,54 @@ export default React.createClass({
     let amount = this.data.balance ? this.data.balance.amount / Math.pow(10, 8) : 0;
     return amount.toFixed(8);
   },
-  showWithdraw(id){
+  showWithdraw(id) {
     Dispatcher.dispatch({actionType: 'SET_WITHDRAWAL_CURRENCY', payload: this.props.current});
     Dispatcher.dispatch({actionType: 'SHOW_WITHDRAW_MODAL'});
   },
   renderWalletItems() {
     return this.data.currencies.map((item) => {
-      var address = this.getAddress(item._id);
-      var balance = this.getBalance(item._id);
+      let address = this.getAddress(item._id);
+      let balance = this.getBalance(item._id);
       return  (
 
         <tr key={item._id}>
-          <td className="two wide">{balance}</td>
-          <td className="two wide">{item.shortName}</td>
-          <td className="nine wide">
+          <td className='two wide'>{balance}</td>
+          <td className='two wide'>{item.shortName}</td>
+          <td className='nine wide'>
             { address ?
               address :
-              <button className="ui mini button" onClick={this.newWallet.bind(this, item)}>Generate</button> }
+              <button className='ui mini button' onClick={this.newWallet.bind(this, item)}>Generate</button> }
           </td>
-          <td className="three wide right aligned">
-            <div className="ui mini buttons">
-              <a className={"ui blue button" + (balance>0?'':" disabled")} onClick={this.showWithdraw} >
+          <td className='three wide right aligned'>
+            <div className='ui mini buttons'>
+              <a className={'ui blue button' + (balance>0?'':' disabled')} onClick={this.showWithdraw} >
                 Withdraw
               </a>
-              <a className="ui button" href={"/u/wallet/" + item._id}>
+              <a className='ui button' href={'/u/wallet/' + item._id}>
                 Details
               </a>
             </div>
           </td>
         </tr>
-
       );
-
     });
   },
   render() {
     return (
       <tr key={this.props.item._id}>
-        <td className="two wide">{balance}</td>
-        <td className="two wide">{this.props.item.shortName}</td>
-        <td className="nine wide">
+        <td className='two wide'>{balance}</td>
+        <td className='two wide'>{this.props.item.shortName}</td>
+        <td className='nine wide'>
           { address ?
             address :
-            <button className="ui mini button" onClick={this.newWallet.bind(this, item)}>Generate</button> }
+            <button className='ui mini button' onClick={this.newWallet.bind(this, item)}>Generate</button> }
         </td>
-        <td className="three wide right aligned">
-          <div className="ui mini buttons">
-            <a className={"ui blue button" + (balance>0?'':" disabled")} onClick={this.showWithdraw} >
+        <td className='three wide right aligned'>
+          <div className='ui mini buttons'>
+            <a className={'ui blue button' + (balance>0?'':' disabled')} onClick={this.showWithdraw} >
               Withdraw
             </a>
-            <a className="ui button" href={"/u/wallet/" + item._id}>
+            <a className='ui button' href={'/u/wallet/' + item._id}>
               Details
             </a>
           </div>
