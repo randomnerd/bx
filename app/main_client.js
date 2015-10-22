6ce9ad06cfabdeba5adbb8d2504a010ed241d22c
Random Nerd <randomnerd@sickoffice.net>
2015-10-22 14:39:30 +0300

    fix loading order

--- app/main_client.js
+++ app/main_client.js
@@ -2,6 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 window.React = React;
 window.ReactDOM = ReactDOM;
-import './routes';
 import './models';
+import './routes';
 import './startup/user_subscriptions';
