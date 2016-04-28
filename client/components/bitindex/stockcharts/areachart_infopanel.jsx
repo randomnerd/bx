18dab50770f5a896dc2f0575753faa37523ba637
Johndoe <johndoe@sickoffice.net>
2016-04-28 19:35:09 +0300

    area chart fix

--- client/components/bitindex/stockcharts/areachart_infopanel.jsx
+++ client/components/bitindex/stockcharts/areachart_infopanel.jsx
@@ -1,21 +1,15 @@
 'use strict';
 
 import React from 'react';
-import d3 from 'd3';
 
+import d3 from 'd3';
 import ReStock from 'react-stockcharts';
 
 let {ChartCanvas, Chart} = ReStock;
 
-let {BarSeries, AreaSeries,} = ReStock.series;
-let {financeEODDiscontiniousScale} = ReStock.scale;
+let {AreaSeries} = ReStock.series;
 
-
-
-let {XAxis, YAxis} = ReStock.axes;
 let {fitWidth} = ReStock.helper;
-
-let xScale = financeEODDiscontiniousScale();
 
 class areachart_infopanel extends React.Component {
     render() {
@@ -26,16 +20,11 @@ class areachart_infopanel extends React.Component {
                 right: 0,
                 top: 0,
                 bottom: 0
-            }} type={type} seriesName='MSFT' data={data} xAccessor={d => d.date} discontinous xScale={xScale} xExtents={[
-                new Date(2012, 0, 1),
-                new Date(2012, 2, 2)
-            ]}>
+            }} type={type} data={data} xAccessor={d => d.date} xScale={d3.time.scale()}>
                 <Chart id={1} yExtents={d => d.close}>
                     <AreaSeries yAccessor={d => d.close}/>
                 </Chart>
-
             </ChartCanvas>
-
         );
     }
 }
