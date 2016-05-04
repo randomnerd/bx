7aab5e8fe2dfb50eb33ea5823660f693539943a8
1st_MAN <1st_MAN@mail.ru>
2016-05-04 16:34:46 +0300

    fix
    This reverts commit e69c66bf0423d5d685774cff080efb3eb2b17289.

--- both/models/transaction.jsx
+++ both/models/transaction.jsx
@@ -1,17 +1,17 @@
 import {Transactions} from '../collections';
 
 Transactions._transform = (obj) => {
-    return new Transaction(obj);
+  return new Transaction(obj);
 };
 
 class Transaction {
-    constructor(data) {
-        _.extend(this, data);
-    }
-    displayAmount() {
-        return (this.amount / Math.pow(10, 8)).toFixed(8);
-    }
-    displayChanged() {
-        return (this.changed / Math.pow(10, 8)).toFixed(8);
-    }
+  constructor(data) {
+    _.extend(this, data);
+  }
+  displayAmount() {
+    return (this.amount / Math.pow(10, 8)).toFixed(8);
+  }
+  displayChanged() {
+    return (this.changed / Math.pow(10, 8)).toFixed(8);
+  }
 }
