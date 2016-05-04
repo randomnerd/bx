38a49f4172063811c593c46683355a0a05426918
Johndoe <johndoe@sickoffice.net>
2016-05-04 17:55:13 +0300

    chart: grid, zoom,pan, etc

--- client/components/bitindex/stockcharts/candelstick_default.jsx
+++ client/components/bitindex/stockcharts/candelstick_default.jsx
@@ -5,53 +5,84 @@ import d3 from 'd3';
 import ReStock from '/client/lib/react-stockcharts';
 
 const CandleStickChart = React.createClass({
-  propTypes: {
-    data: React.PropTypes.array.isRequired,
-    width: React.PropTypes.number.isRequired,
-    type: React.PropTypes.oneOf(['svg', 'hybrid']).isRequired
-  },
+    propTypes: {
+        data: React.PropTypes.array.isRequired,
+        width: React.PropTypes.number.isRequired,
+        type: React.PropTypes.oneOf(['svg', 'hybrid']).isRequired
+    },
 
-  defaultProps: {
-    type: 'svg'
-  },
+    defaultProps: {
+        type: 'svg'
+    },
 
-  render() {
-    let {ChartCanvas, Chart} = ReStock;
-    let {CandlestickSeries, BarSeries} = ReStock.series;
-    let {financeEODDiscontiniousScale} = ReStock.scale;
+    render() {
+        let {ChartCanvas, Chart, EventCapture} = ReStock;
 
-    let {XAxis, YAxis} = ReStock.axes;
+        let {CandlestickSeries, BarSeries} = ReStock.series;
 
-    let {fitWidth} = ReStock.helper;
+        let {financeEODDiscontiniousScale} = ReStock.scale;
 
-    let {data, type, width} = this.props;
-    if (data.length > 300) data = _.last(data, 300);
+        let {TooltipContainer, OHLCTooltip} = ReStock.tooltip;
 
-    return (
-        <ChartCanvas width={width} height={350} margin={{
+        let { MouseCoordinates, CurrentCoordinate } = ReStock.coordinates;
+
+        let {XAxis, YAxis} = ReStock.axes;
+
+        let {fitWidth} = ReStock.helper;
+
+        let {data, type, width} = this.props;
+        let height = 350;
+        let margin = {
             left: 30,
             right: 30,
             top: 0,
             bottom: 20
-        }} type={type} seriesName='MSFT' data={data} xAccessor={d => d.date} discontinous xScale={financeEODDiscontiniousScale()} >
-            <Chart id={1} height={350} yExtents={d => [d.high, d.low]}>
-                <YAxis axisAt='left' orient='left' ticks={5}  fontSize={10} stroke='#767676' tickStroke='#767676' t/>
+        };
+        let gridHeight = height - margin.top - margin.bottom;
+        let gridWidth = width - margin.left - margin.right;
 
-                <CandlestickSeries/>
-            </Chart>
-            <Chart id={2} origin={(w, h) => [
-                0, h - 150
-            ]} height={150} yExtents={d => d.volume}>
-                <XAxis axisAt='bottom' orient='bottom' fontSize={10} stroke='#767676' tickStroke='#767676'/>
-                <YAxis axisAt='right' orient='right' ticks={5} fontSize={10} stroke='#767676' tickStroke='#767676' tickFormat={d3.format('s')}/>
+        let showGrid = true;
+        let yGrid = showGrid
+            ? {
+                innerTickSize: -1 * gridWidth,
+                tickStrokeOpacity: 0.2
+            }
+            : {};
+        let xGrid = showGrid
+            ? {
+                innerTickSize: -1 * gridHeight,
+                tickStrokeOpacity: 0.2
+            }
+            : {};
 
-                <BarSeries yAccessor={d => d.volume} fill={(d) => d.close > d.open
-                    ? '#21ba45'
-                    : '#db2828'}/>
-            </Chart>
-        </ChartCanvas>
-    );
-  }
+        if (data.length > 300)
+            data = _.last(data, 300);
+
+        return (
+            <ChartCanvas width={width} height={height} margin={margin} type={type} seriesName='MSFT' data={data} xAccessor={d => d.date} discontinous xScale={financeEODDiscontiniousScale()}>
+                <Chart id={1} height={350} yExtents={d => [d.high, d.low]}>
+                    <YAxis axisAt='left' orient='left' ticks={5} fontSize={10} stroke='#767676' tickStroke='#767676' {...yGrid}/>
+                    <CandlestickSeries/>
+                </Chart>
+                <Chart id={2} origin={(w, h) => [
+                    0, h - 150
+                ]} height={150} yExtents={d => d.volume}>
+                    <XAxis axisAt='bottom' orient='bottom' fontSize={10} stroke='#767676' tickStroke='#767676'/>
+                    <YAxis axisAt='right' orient='right' ticks={5} fontSize={10} stroke='#767676' tickStroke='#767676' tickFormat={d3.format('s')}/>
+
+                    <BarSeries yAccessor={d => d.volume} fill={(d) => d.close > d.open
+                        ? '#21ba45'
+                        : '#db2828'}/>
+                </Chart>
+                <MouseCoordinates xDisplayFormat={d3.time.format("%Y-%m-%d")}/>
+                <EventCapture mouseMove={true} zoom={true} pan={true}/>
+                <TooltipContainer>
+                    <OHLCTooltip forChart={1} origin={[10, 5]}/>
+
+                </TooltipContainer>
+            </ChartCanvas>
+        );
+    }
 });
 
 export default ReStock.helper.fitWidth(CandleStickChart);
