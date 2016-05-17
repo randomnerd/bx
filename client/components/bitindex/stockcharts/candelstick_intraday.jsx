51752e0ae480d1dd6a8b7b710f96a473f38aff2a
Johndoe <johndoe@sickoffice.net>
2016-05-17 18:04:21 +0300

    brush add

--- client/components/bitindex/stockcharts/candelstick_intraday.jsx
+++ client/components/bitindex/stockcharts/candelstick_intraday.jsx
@@ -5,7 +5,10 @@ import d3 from 'd3';
 
 import ReStock from '/client/lib/react-stockcharts';
 
+const BRUSH_TYPE = "1D";
+
 const candelstick_intraday = React.createClass({
+
     propTypes: {
         data: React.PropTypes.array.isRequired,
         width: React.PropTypes.number.isRequired,
@@ -14,7 +17,40 @@ const candelstick_intraday = React.createClass({
     defaultProps: {
         type: 'svg'
     },
+    componentDidMount() {
+        document.addEventListener("keyup", this.onKeyPress);
+    },
+    componentWillUnmount() {
+        document.removeEventListener("keyup", this.onKeyPress);
+    },
+    onKeyPress(e) {
+        let keyCode = e.which;
+        console.log(keyCode);
+        switch (keyCode) {
+            case 27:
+                { // ESC
+                    this.refs.brush.getWrappedComponent().terminate();
+                }
+        }
+    },
+    handleBrush(brushCoords, startAndEndItem, startAndEndMouseLocation, event) {
+        let left = Math.min(brushCoords.x1, brushCoords.x2);
+        let right = Math.max(brushCoords.x1, brushCoords.x2);
 
+        let low = Math.min(brushCoords.y1, brushCoords.y2);
+        let high = Math.max(brushCoords.y1, brushCoords.y2);
+
+        console.log(arguments);
+        // uncomment the line below to make the brush to zoom
+        this.setState({
+            xExtents: [
+                left, right
+            ],
+            yExtents: BRUSH_TYPE === "2D"
+                ? [low, high]
+                : this.state.yExtents
+        })
+    },
     tooltipContent(calculators) {
 
         let dateFormat = d3.time.format('%Y-%m-%d');
@@ -61,10 +97,11 @@ const candelstick_intraday = React.createClass({
         let {TooltipContainer, OHLCTooltip, MovingAverageTooltip, HoverTooltip} = ReStock.tooltip;
         let {XAxis, YAxis} = ReStock.axes;
         let {ema, sma, change} = ReStock.indicator;
-        let { Annotate, LabelAnnotation, Label } = ReStock.annotation;
+        let {Annotate, LabelAnnotation, Label} = ReStock.annotation;
         let {fitWidth} = ReStock.helper;
         let {data, type, width} = this.props;
 
+        let {Brush} = ReStock.interactive;
         let ema20 = ema().id(0).windowSize(20).merge((d, c) => {
             d.ema20 = c;
         }).accessor(d => d.ema20);
@@ -75,22 +112,35 @@ const candelstick_intraday = React.createClass({
             d.smaVolume50 = c;
         }).accessor(d => d.smaVolume50);
 
+        this.state = {
+            xExtents: [
+                new Date(2012, 0, 3),
+                new Date(2012, 5, 29)
+            ],
+            yExtents: [
+                [d => d.volume],
+                ema20.accessor(),
+                ema50.accessor()
+            ]
+        }
         let changeCalculator = change();
 
         let annotationProps = {
-            fontFamily: 'Glyphicons Halflings',
+            fontFamily: 'opensans',
             fontSize: 20,
             fill: '#060F8F',
             opacity: 0.8,
             text: '\ue093',
             y: ({yScale}) => (yScale.range()[0] - 10)
         };
+
         let margin = {
-          left: 40,
-          right: 80,
-          top: 10,
-          bottom: 30
+            left: 40,
+            right: 80,
+            top: 10,
+            bottom: 30
         }
+
         let height = 350;
 
         let [yAxisLabelX,
@@ -100,13 +150,10 @@ const candelstick_intraday = React.createClass({
         ];
 
         return (
-            <ChartCanvas width={width} height={height} margin={margin} type={type} seriesName='MSFT' data={data} calculator={[ema20, ema50, changeCalculator]} xExtents={[
-                new Date(2014, 9, 1),
-                new Date(2015, 2, 2)
-            ]} xAccessor={d => d.date} xScaleProvider={discontinuousTimeScaleProvider}>
+            <ChartCanvas width={width} height={height} margin={margin} type={type} seriesName='MSFT' data={data} calculator={[ema20, ema50, changeCalculator]} xExtents={this.state.xExtents} xAccessor={d => d.date} xScaleProvider={discontinuousTimeScaleProvider}>
                 <Label x={(width - margin.left - margin.right) / 2} y={30} fontSize='30' text='BTC USD'/>
 
-                <Chart id={2} yExtents={[d => d.volume]} yMousePointerDisplayLocation='left' yMousePointerDisplayFormat={d3.format('.4s')} height={100} origin={(w, h) => [
+                <Chart id={2} yExtents={this.state.yExtents} yMousePointerDisplayLocation='left' yMousePointerDisplayFormat={d3.format('.4s')} height={100} origin={(w, h) => [
                     0, h - 100
                 ]}>
                     <YAxis axisAt='left' orient='left' ticks={5} tickFormat={d3.format('s')}/>
@@ -136,8 +183,11 @@ const candelstick_intraday = React.createClass({
 
                     <VolumeProfileSeries orient='left'/>
                     <CandlestickSeries/>
+
+                    <Brush ref="brush" id={0} enabled={true} type={BRUSH_TYPE} onStart={e => console.log("Start Event:", e)} onBrush={this.handleBrush}/>
                     <LineSeries yAccessor={ema20.accessor()} stroke={ema20.stroke()}/>
                     <LineSeries yAccessor={ema50.accessor()} stroke={ema50.stroke()}/>
+
                     <EdgeIndicator itemType='last' orient='right' edgeAt='right' yAccessor={d => d.close} fill={d => d.close > d.open
                         ? '#6BA583'
                         : '#FF0000'}/>
