707ea791dd04a060ded5d5647b580d95a2588780
Random Nerd <randomnerd@sickoffice.net>
2016-06-07 17:41:34 +0300

    never publish secret keys

--- server/publish/wallets.js
+++ server/publish/wallets.js
@@ -2,5 +2,5 @@ import {Wallets} from '/both/collections';
 
 Meteor.publish('wallets', function() {
   if (!this.userId) return;
-  return Wallets.find({userId: this.userId});
+  return Wallets.find({userId: this.userId}, {fields: {secret: 0}});
 });
