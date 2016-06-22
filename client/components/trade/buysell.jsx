f95a3c2ec0ee57dc0e890d741763faca6b41d21c
1st_MAN <1st_MAN@mail.ru>
2016-06-22 18:45:25 +0300

    buysell bug

--- client/components/trade/buysell.jsx
+++ client/components/trade/buysell.jsx
@@ -21,8 +21,8 @@ const BuySell = Component({
 
     let params = {
       pairId: this.props.pair._id,
-      amount: parseFloat((this.props.buysell.amount).replace(',','.')),
-      price:  parseFloat((this.props.buysell.price).replace(',','.')),
+      amount: parseFloat(this.props.buysell.amount),
+      price:  parseFloat(this.props.buysell.price),
       buy:    buy
     };
     Meteor.call('createOrder', params);
