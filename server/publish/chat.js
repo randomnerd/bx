8b7d9b53f70de30f4729a3e987bdc95bb7863acd
Random Nerd <randomnerd@sickoffice.net>
2016-07-20 15:08:46 +0300

    fix private chat publish

--- server/publish/chat.js
+++ server/publish/chat.js
@@ -1,5 +1,14 @@
 import {Chat} from '/both/collections';
 
 Meteor.publish('chat', function() {
-  return Chat.find({}, {sort: {createdAt: -1}, limit: 50});
+  return Chat.find({
+    $or: [
+      { isPrivate: false },
+      { replyId: this.userId },
+      { userId: this.userId }
+    ]
+  }, {
+    sort: { createdAt: 1 },
+    limit: 50
+  });
 });
