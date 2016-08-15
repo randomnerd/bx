576b1bca24f6b776dda6191af336210c5cca5b27
Random Nerd <randomnerd@sickoffice.net>
2016-08-15 18:37:27 +0300

    profile.[loginHistory, online]

---
+++ server/login_history.js
@@ -0,0 +1,9 @@
+Accounts.onLogin(function(session) {
+  let {user, connection}  = session;
+  let loginHistory = user.profile.loginHistory || [];
+  let addr = connection.httpHeaders['x-forwarded-for'] || connection.clientAddress;
+  let newRecord = {time: new Date, addr};
+  if (_.isEqual(loginHistory[0], newRecord)) return;
+  loginHistory.unshift(newRecord);
+  Meteor.users.update({ _id: user._id }, { $set: { 'profile.loginHistory': loginHistory.slice(0, 10) } });
+});
