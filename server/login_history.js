bb8b6b9c549d2a7d243d4c1dd2f06c462313d08e
Random Nerd <randomnerd@sickoffice.net>
2016-08-18 15:50:16 +0300

    fix loginhistory with no user.profile

--- server/login_history.js
+++ server/login_history.js
@@ -1,6 +1,6 @@
 Accounts.onLogin(function(session) {
   let {user, connection}  = session;
-  let loginHistory = user.profile.loginHistory || [];
+  let loginHistory = user.profile && user.profile.loginHistory || [];
   let addr = connection.httpHeaders['x-forwarded-for'] || connection.clientAddress;
   let newRecord = {time: new Date, addr};
   if (_.isEqual(loginHistory[0], newRecord)) return;
